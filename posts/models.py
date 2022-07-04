29302559d031aac50e273655e8272db55abb1c8c
miguelMarch <miguelaugusto179@gmail.com>
2022-07-04 12:51:21 +0200

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Post(models.Model):
+    text = models.TextField()
+    num = models.IntegerField()
+    def __str__(self):
+        return f"{self.text[:50]}{self.num}"
